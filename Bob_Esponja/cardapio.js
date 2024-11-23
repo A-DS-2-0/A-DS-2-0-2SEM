3ba1a8cd4ede90e3d2057bab15fdf9b2d5c0eb89
Thais Oliveira <thaisopereira02@gmail.com>
2024-11-23 13:39:00 -0300

    Add files via upload

---
+++ Bob_Esponja/cardapio.js
@@ -0,0 +1,50 @@
+function Calcular() {
+    const items = {
+        x_plankton: { name: 'X-Plankton', price: 50.00 },
+        bobburguer: { name: 'Bob Burguer', price: 50.00 },
+        hamburguersiri: { name: 'Hambúrguer de Siri', price: 55.00 },
+        hambuguerlulamolusco: { name: 'Hambúrguer Lula Molusco', price: 52.00 },
+        x_Patrick: { name: 'X-Patrick', price: 58.00 },
+        sandyburguer: { name: 'Sandy Burguer', price: 59.00 },
+        combo_siricascudo: { name: 'Combo_siricascudo', price: 75.00 },
+        combo_fendadobiquini: { name: 'Combo_fendadobiquini', price: 80.00 },
+        combo_patrickestrela: { name: 'Combo Bacon Deluxe', price: 85.00 },
+        combo_sandycheddar: { name: 'Combo Vegetariano', price: 70.00 },
+        combo_lulamolusco: { name: 'combo_lulamolusco', price: 120.00 },
+        combo_bobkids: { name: 'Combo Kids', price: 50.00 },
+        fanta_laranja: { name: 'Fanta Laranja', price: 5.00 },
+        coca_cola: { name: 'Coca Cola', price: 5.00 },
+        guarana: { name: 'Guaraná', price: 5.00 },
+        suco_uva: { name: 'Suco de Uva', price: 6.00 },
+        suco_laranja: { name: 'Suco de Laranja', price: 6.00 },
+        suco_maracuja: { name: 'Suco de Maracujá', price: 10.00 }
+    };
+    
+    
+    let total = 0;
+    let receipt = '';
+    
+    for (let item in items) {
+        let quantity = document.getElementById(item).value;
+        if (quantity && !isNaN(quantity) && parseInt(quantity) > 0) {
+            let itemTotal = items[item].price * parseInt(quantity);
+            total += itemTotal;
+            receipt += `<li>${items[item].name} (x${quantity}) - R$${itemTotal.toFixed(2)}</li>`;
+        }
+    }
+    if (total!= 0){
+
+        receipt += `</ul><h4>Total: R$${total.toFixed(2)}</h4>`;
+        document.getElementById('total').innerHTML = receipt;
+    }else{
+        document.getElementById('total').innerHTML = `<h3>Selecione algum produto</h3>`;
+        document.getElementById('confirmar_pedido').style.visibility="hidden"; 
+        document.getElementById('fechar_modal').innerText = "Ok" 
+
+    }
+}
+
+function confirmar_pedido() {
+    alert('Pedido Confirmado!')
+    location.reload()
+}
---
+++ Bob_Esponja/index.php
@@ -0,0 +1,123 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+    <link rel="stylesheet" href="css/style.css">
+    <script src="script/cardapio.js"></script>
+    <title>SPONGE BOB BURGUER</title>
+</head>
+<body>
+    <header> 
+        <nav id="nav_bar" class="navbar bg-body-tertiary">
+            <div id="container" class="container">
+              <a class="navbar-brand" href="#">
+                <img id="logo" src="img/logo1.png" alt="logo" width="40" height="40">
+              </a>
+              <a href="#historia">HISTORIA</a>
+              <a href="#cardapio">CARDAPIO</a>
+              <a  id="cardapio" href="bob_esponja/login.php" class="login-button">LOGIN</a>
+            </div>
+          </nav>
+    <nav>
+    </header>
+
+<br>
+<br>
+    <section class="container">
+        <figure>
+            <img id="bob_gif" class="imagens-site" src="img/bob2.gif" alt="siri´s burguer">
+        </figure>
+        <h2>Um hambúrguer tão delicioso que até o Plankton vai querer roubar!</h2>
+    </section>
+
+<br>
+<hr>
+<br>
+<section class="container">
+<section class="texto-imagem">
+    <div class="texto">
+    <a href="#historia" id="Historia"></a>
+        <p>
+        Direto da Fenda do Biquíni, o Hambúrguer de Siri é um clássico que dispensa apresentações. 
+        Criado pelo esperto Sr. Sirigueijo, esse lanche é a sensação do momento e o motivo pelo qual o Krusty Krab é sempre lotado. 
+        Se você é fã de Bob Esponja ou simplesmente adora um bom hambúrguer, prepare-se para descobrir por que esse lanche é tão especial!
+        </p>
+    </div>
+    <div class="imagem">
+        <img src="img/restaurante.jpg" alt="bob" />
+    </div>
+</section>
+<br>
+<br>
+<section class="texto-imagem2">
+    <div class="texto2">
+        <p>
+        A fórmula exata do Hambúrguer de Siri, a estrela do Siri Cascudo, é um dos segredos mais bem guardados da Fenda do Biquíni. Sr. Sirigueijo, o proprietário do restaurante, guarda a receita a sete chaves para garantir o sucesso de seu negócio e manter Plankton, seu rival, sempre frustrado.
+A criação do hambúrguer é envolta em mistério. Sabe-se que Sr. Sirigueijo passou anos aperfeiçoando a receita, experimentando diferentes combinações de ingredientes até chegar ao sabor único e irresistível que conquistou o paladar de Bob Esponja e de todos os habitantes da cidade submarina.
+A lenda do Hambúrguer de Siri se espalhou por todos os oceanos, tornando-o um ícone da culinária fast-food e um objeto de desejo para muitos. Apesar de todos os esforços de Plankton para desvendar o segredo, o Hambúrguer de Siri continua sendo um mistério, alimentando a curiosidade de fãs ao redor do mundo.
+        </p>
+    </div>
+    <div class="imagem2">
+        <img src="img/receita.png" alt="receita" />
+    </div>
+</section>
+</section>
+<br>
+<br>
+<hr>
+
+<!-- CARDAPIO -->
+<section class="conteudo" id="grid-conteudo">
+<a href="#cardapio" id="cardapio"></a>
+    <h1> CARDAPIO </h1>
+    <br>
+    <div class="container">
+        <a href="#lanches" id="LANCHES"></a>
+         <br>
+         <div id="lanches" class="row mb-4">
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="img/x- Plankton.png" class="card-img-top" alt="Hambuguer X-Plankton">
+                    <div class="card-body">
+                        <h3 class="card-title">X- Plankton</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="img/bobburguer.png" class="card-img-top" alt="Hambuguer Bob Burguer">
+                    <div class="card-body">
+                        <h3 class="card-title">Bob Burguer</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <img src="img/hamburguerSiri.png" class="card-img-top" alt="Hamburguer de Siri">
+                    <div class="card-body">
+                        <h3 class="card-title">Hamburguer de Siri</h3>
+                    </div>
+                </div>
+            </div>
+<br>
+<h2>
+    Para ter acesso completo ao nosso cardápio e se deliciar com nossos hambúrgueres, faça o seu login.
+</h2>
+
+<br>
+<br>
+
+    <footer> 
+        <br>
+        <h4>SPONGE BOOB BURGUER</h4>
+        <h5>Hamburgueria Artesanal</h5>
+        <h6> Erick Fernado | Thais Oliveria </h6>
+    </footer>
+</body>
+</html>
+
+
